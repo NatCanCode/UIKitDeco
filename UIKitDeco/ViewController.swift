efaff1c652f7025815949dac4e778fbd32b3091d
NatCanCode <nathalie.piat1@gmail.com>
2024-02-12 12:30:46 +0100

    Add constraints in Storyboard

--- UIKitDeco/ViewController.swift
+++ UIKitDeco/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var image: UIImageView! // Rename imageView + reconnect link
+    let images = [
+        "img1",
+        "img2",
+        "img3",
+        "img4"
+    ]
 
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label: UILabel! // Rename descriptionLabel + reconnect link
+    let descriptions = [
+        "Fauteuil élégant accompagné de son repose pieds jaune orangé",
+        "Ce luminaire est tout simplement parfait pour sublimer votre table a manger ou un salon",
+        "Chaise blanche de style scandinave",
+        "Sofa vert en velou"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +31,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonLeft(_ sender: Any) {
+        print("Left button pressed")
+        // image.image = UIImage(named: )
 
     }
     
     @IBAction func buttonRight(_ sender: Any) {
-        
+        print("Right button pressed")
     }
 }
 
